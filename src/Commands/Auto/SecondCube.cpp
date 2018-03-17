3c12657ee40c38eb874499e42e0d0e80ce93121b
Kevin Hsu <Kevin Hsu@192.168.231.228>
2018-03-17 17:07:30 -0400

    v1.0, Decreased initial backtrack time from .4 to .35 for second cubes

--- src/Commands/Auto/SecondCube.cpp
+++ src/Commands/Auto/SecondCube.cpp
@@ -7,7 +7,7 @@
 
 #include <Commands/Auto/SecondCube.h>
 
-SecondCube::SecondCube(bool left) : m_moveBack(-0.3, -0.3, 400), m_moveGrabberDown(0.6, false, 3000),
+SecondCube::SecondCube(bool left) : m_moveBack(-0.3, -0.3, 350), m_moveGrabberDown(0.6, false, 3000),
 									m_turnRightAngle(left ? 85.0 : -85.0, 0.0, ETA_SPOT, 3000), m_spinGrabberLong(-1.0, 1.0, 2000),
 									m_moveForward(0.4, 0.4, 2000), m_moveBackLong(-0.4, -0.4, 2000),
 									m_turnRightAngle2(left ? -85.0 : 85.0, 0.2, ETA_SPOT, 1000),
